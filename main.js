ed30bd68dd213b3bfc3e43727e008c257a2e0703
Danil-patel <79022114+Danil-patel@users.noreply.github.com>
2021-11-19 16:55:31 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 img = "";
+myStatus = '';
 
 function preload(){
     img = loadImage("dog_cat.jpg");
@@ -7,6 +8,8 @@ function preload(){
 function setup(){
     canvas = createCanvas(640,420);
     canvas.center();
+    objectDetector = ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML = 'status: detecting objects';
 }
 
 function draw(){
@@ -16,4 +19,25 @@ function draw(){
     noFill();
     stroke("#FF0000");
     rect(30,60,450,350);
+    
+    fill("#FF0000");
+    text("cat",320,120);
+    noFill();
+    stroke("#FF0000");
+    rect(300,90,270,320);
+}
+
+function modelLoaded(){
+    console.log('model is loaded');
+    myStatus = true;
+    objectDetector.detect(img, gotResult);
+}
+
+function gotResult(error, results){
+    if(error) {
+        console.log(error);
+    }
+    else{
+        console.log(results);
+    }
 }
