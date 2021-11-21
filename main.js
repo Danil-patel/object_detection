c1301fafcf9eccf7990badb6cb766c4c59a8bd77
Danil-patel <79022114+Danil-patel@users.noreply.github.com>
2021-11-21 16:26:16 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 img = "";
 myStatus = '';
+objects = [];
 
 function preload(){
     img = loadImage("dog_cat.jpg");
@@ -14,17 +15,19 @@ function setup(){
 
 function draw(){
     image(img,0,0,640,420);
-    fill("#FF0000");
-    text("dog",45,75);
-    noFill();
-    stroke("#FF0000");
-    rect(30,60,450,350);
+    if(myStatus !=""){
+        for(i = 0 ; i<objects.length ; i++){
+            document.getElementById("status").innerHTML = 'status: detecting objects';
+            fill("#FF0000");
+            percent = floor(objects[i].confidence*100);
+            text(objects[i].label + " " + percent + " %",objects[i].x,objects[i].y);
+            noFill();
+            stroke("#FF0000");
+            rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);
+        }
+    }
     
-    fill("#FF0000");
-    text("cat",320,120);
-    noFill();
-    stroke("#FF0000");
-    rect(300,90,270,320);
+    
 }
 
 function modelLoaded(){
@@ -39,5 +42,6 @@ function gotResult(error, results){
     }
     else{
         console.log(results);
+        objects = results;
     }
 }
